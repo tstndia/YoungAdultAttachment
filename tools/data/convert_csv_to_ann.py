4fc8d1cf1722c6fed679ec2c12e6ceb5633d07ee
Fazry <lhuqita.fazry@gmail.com>
2022-11-14 20:11:43 +0700

    a

--- tools/data/convert_csv_to_ann.py
+++ tools/data/convert_csv_to_ann.py
@@ -25,7 +25,7 @@ def convert_csv_to_ann(csv_path):
     for index, video in enumerate(data_train):
         for idx, emotion in enumerate(emotions):
             if int(label_train[index][idx]) == 1:
-                class_emotions.append(idx + 1)
+                class_emotions.append(idx)
 
         items = " ".join(map(str, class_emotions))
         train_rows.append(f"{video[0]} {items}")
@@ -34,7 +34,7 @@ def convert_csv_to_ann(csv_path):
     for index, video in enumerate(data_test):
         for idx, emotion in enumerate(emotions):
             if int(label_test[index][idx]) == 1:
-                class_emotions.append(idx + 1)
+                class_emotions.append(idx)
 
         items = " ".join(map(str, class_emotions))
         val_rows.append(f"{video[0]} {items}")
