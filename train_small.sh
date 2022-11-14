de6385fd06791b38b6594b338785c93e7f32cc69
Fazry <lhuqita.fazry@gmail.com>
2022-11-14 20:47:44 +0700

    add new script

---
+++ train_small.sh
@@ -0,0 +1,1 @@
+CUDA_VISIBLE_DEVICES=$CUDA_ID python tools/train.py configs/recognition/attachment/swin_small_patch244_window877_kinetics400_1k.py --cfg-options model.backbone.pretrained=pretrained/swin_small_patch4_window7_224.pth
