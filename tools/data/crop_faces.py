30054c653039dc5d622888b44d767182fd2896be
Fazry <lhuqita.fazry@gmail.com>
2022-11-15 12:44:31 +0700

    add crop videos

--- tools/data/crop_faces.py
+++ tools/data/crop_faces.py
@@ -134,15 +134,19 @@ def crop_faces(input_dir, output_dir, detector, dim):
         faces = []
 
         for i in range(frames.shape[0]):
-            frame = imutils.resize(frames[i,:,:,:].squeeze(), height=256)
+            try:
+                frame = imutils.resize(frames[i,:,:,:].squeeze(), height=256)
                 
-            face = DeepFace.detectFace(img_path = frame, 
-                target_size = dim,
-                detector_backend = detector,
-                align = False
-            )
+                face = DeepFace.detectFace(img_path = frame, 
+                    target_size = (dim, dim),
+                    detector_backend = detector,
+                    align = False
+                )
 
-            faces.append((face * 255).astype(np.uint8))
+                faces.append((face * 255).astype(np.uint8))
+            except Exception as e:
+                logging.info(f"No face detected on frame: {i}. Skipping ==> {e}")
+                # pass
 
         if len(faces) > 0:
             cropped = np.stack(faces, axis=0)
