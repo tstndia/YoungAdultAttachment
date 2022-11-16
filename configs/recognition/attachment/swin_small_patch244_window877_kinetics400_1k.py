d3ad92c04b7dc3483bae503b7e7f1fbe2f717dd1
tustynadia <tustynadia@gmail.com>
2022-11-16 21:07:01 +0700

    25610

--- configs/recognition/attachment/swin_small_patch244_window877_kinetics400_1k.py
+++ configs/recognition/attachment/swin_small_patch244_window877_kinetics400_1k.py
@@ -14,7 +14,7 @@ img_norm_cfg = dict(
     mean=[123.675, 116.28, 103.53], std=[58.395, 57.12, 57.375], to_bgr=False)
 train_pipeline = [
     dict(type='DecordInit'),
-    dict(type='SampleFrames', clip_len=512, frame_interval=2, num_clips=1),
+    dict(type='SampleFrames', clip_len=256, frame_interval=10, num_clips=1),
     dict(type='DecordDecode'),
     dict(type='Resize', scale=(-1, 64)),
     #dict(type='RandomResizedCrop'),
@@ -29,8 +29,8 @@ val_pipeline = [
     dict(type='DecordInit'),
     dict(
         type='SampleFrames',
-        clip_len=512,
-        frame_interval=2,
+        clip_len=256,
+        frame_interval=10,
         num_clips=1,
         test_mode=True),
     dict(type='DecordDecode'),
@@ -46,8 +46,8 @@ test_pipeline = [
     dict(type='DecordInit'),
     dict(
         type='SampleFrames',
-        clip_len=512,
-        frame_interval=2,
+        clip_len=256,
+        frame_interval=10,
         num_clips=1,
         test_mode=True),
     dict(type='DecordDecode'),
@@ -108,11 +108,11 @@ lr_config = dict(
     warmup_by_epoch=True,
     warmup_iters=2.5
 )
-total_epochs = 30
+total_epochs = 100
 
 # runtime settings
 checkpoint_config = dict(interval=1)
-work_dir = './work_dirs/swin_small_patch244_window877_kinetics400_1k.py'
+work_dir = './work_dirs/attachment_exposure_small_cl256_fi10_b8_ep100'
 find_unused_parameters = False
 
 
