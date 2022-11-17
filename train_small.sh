af942fc78fd973be7a02e01750e140cec2595869
Fazry <lhuqita.fazry@gmail.com>
2022-11-17 10:45:49 +0700

    rename config

--- train_small.sh
+++ train_small.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-CUDA_VISIBLE_DEVICES=$CUDA_ID python tools/train.py configs/recognition/attachment/swin_small_patch244_window877_kinetics400_1k.py --cfg-options model.backbone.pretrained=pretrained/swin_small_patch4_window7_224.pth --evaluate
+CUDA_VISIBLE_DEVICES=$CUDA_ID python tools/train.py configs/recognition/attachment/attachment_exposure_small.py --cfg-options model.backbone.pretrained=pretrained/swin_small_patch4_window7_224.pth --evaluate
