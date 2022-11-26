27f446e0538bafb37e5a4193286b1d98046ec8db
Fazry <lhuqita.fazry@gmail.com>
2022-11-26 10:46:50 +0700

    add response video

--- configs/recognition/attachment/attachment_response_video_small_cl1024_fi2.py
+++ configs/recognition/attachment/attachment_response_video_small_cl1024_fi2.py
@@ -15,12 +15,12 @@ model=dict(
 )
 
 # dataset settings
-dataset_type = 'VideoDataset'
+dataset_type = 'MultilabelVideoDataset'
 data_root = 'data/response-video/'
 data_root_val = 'data/response-video/'
-ann_file_train = 'data/response-video/video_response_nonetral_train.txt'
-ann_file_val = 'data/response-video/video_response_nonetral_val.txt'
-ann_file_test = 'data/response-video/video_response_nonetral_val.txt'
+ann_file_train = 'data/response-video/video_response_train.txt'
+ann_file_val = 'data/response-video/video_response_val.txt'
+ann_file_test = 'data/response-video/video_response_val.txt'
 img_norm_cfg = dict(
     mean=[123.675, 116.28, 103.53], std=[58.395, 57.12, 57.375], to_bgr=False)
 train_pipeline = [
