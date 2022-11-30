3d429ac610275c5f281e595c4f6528aec8ab6c17
Fazry <lhuqita.fazry@gmail.com>
2022-11-30 10:07:11 +0700

    test

--- configs/recognition/attachment/attachment_response_video_small_cl128_fi8.py
+++ configs/recognition/attachment/attachment_response_video_small_cl128_fi8.py
@@ -72,7 +72,7 @@ test_pipeline = [
     dict(type='ToTensor', keys=['imgs'])
 ]
 data = dict(
-    videos_per_gpu=4,
+    videos_per_gpu=2,
     workers_per_gpu=4,
     val_dataloader=dict(
         videos_per_gpu=1,
@@ -133,7 +133,7 @@ find_unused_parameters = False
 fp16 = None
 optimizer_config = dict(
     type="DistOptimizerHook",
-    update_interval=2,
+    update_interval=1,
     grad_clip=None,
     coalesce=True,
     bucket_size_mb=-1,
