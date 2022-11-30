aaa6f8f3463eeafebf7301aebdc06c13d90b978e
tustynadia <tustynadia@gmail.com>
2022-11-30 14:38:26 +0700

    1284

--- configs/recognition/attachment/attachment_response_video_small_cl128_fi4.py
+++ configs/recognition/attachment/attachment_response_video_small_cl128_fi4.py
@@ -7,7 +7,9 @@ model=dict(
         drop_path_rate=0.1
     ),
     cls_head=dict(
-        num_classes=8
+        num_classes=8,
+        loss_cls=dict(type='BCELossWithLogits', loss_weight=160.0),
+        multi_class=True
     ),
     test_cfg=dict(
         max_testing_views=4
@@ -121,11 +123,11 @@ lr_config = dict(
     warmup_by_epoch=True,
     warmup_iters=2.5
 )
-total_epochs = 20
+total_epochs = 10
 
 # runtime settings
 checkpoint_config = dict(interval=1)
-work_dir = './work_dirs/attachment_exposure_small_cl128_fi4_b8_ep20'
+work_dir = './work_dirs/attachment_exposure_small_cl128_fi4_b8_ep10'
 find_unused_parameters = False
 
 
