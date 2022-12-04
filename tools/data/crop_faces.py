bf664561c62ee44f0bcc105b15909e4eca43c637
Fazry <lhuqita.fazry@gmail.com>
2022-12-04 15:15:21 +0700

    fixing bugs

--- tools/data/crop_faces.py
+++ tools/data/crop_faces.py
@@ -146,7 +146,7 @@ def crop_faces(input_dir, output_dir, detector, dim):
     #pool = ProcessPoolExecutor()
     #pool.submit(lambda: None)
     
-    with multiprocessing.Pool(processes=10, maxtasksperchild=20) as pool:
+    with multiprocessing.Pool(processes=50) as pool:
     #with get_context("spawn").Pool(processes=30) as pool:
         items = [(video, output_dir, detector, dim, idx, len(uncropped_videos)) 
             for idx, video in enumerate(uncropped_videos)]
