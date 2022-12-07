03af14d6664def35f910a2667603a4f1799c1e69
Fazry <lhuqita.fazry@gmail.com>
2022-12-08 05:59:32 +0700

    d

--- configs/recognition/attachment/attachment_response_audio_cl128_fi3.py
+++ configs/recognition/attachment/attachment_response_audio_cl128_fi3.py
@@ -76,7 +76,7 @@ evaluation = dict(
 
 # optimizer
 optimizer = dict(
-    type='SGD', lr=0.01, momentum=0.9,
+    type='SGD', lr=0.001, momentum=0.9,
     weight_decay=0.0001)  # this lr is used for 8 gpus
 optimizer_config = dict(grad_clip=dict(max_norm=40, norm_type=2))
 # learning policy
