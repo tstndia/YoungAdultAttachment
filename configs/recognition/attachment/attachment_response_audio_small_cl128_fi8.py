6ae5961fe5ed95b53abd353c1f3abb7417ef26aa
Fazry <lhuqita.fazry@gmail.com>
2022-12-07 13:54:04 +0700

    d

--- configs/recognition/attachment/attachment_response_audio_small_cl128_fi8.py
+++ configs/recognition/attachment/attachment_response_audio_small_cl128_fi8.py
@@ -7,7 +7,7 @@ model=dict(
     ),
     cls_head=dict(
         num_classes=8,
-        loss_cls=dict(type='BCELossWithLogits'),
+        loss_cls=dict(type='BCELossWithLogits', loss_weight=160.0),
         multi_class=False
     )
 )
