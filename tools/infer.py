1ae76a74f1ea25defa6e83aad9bd2d1d2976072d
Fazry <lhuqita.fazry@gmail.com>
2022-12-13 13:33:11 +0700

    add more text

--- tools/infer.py
+++ tools/infer.py
@@ -279,7 +279,7 @@ def infer(cfg, dataset, distributed, args):
 
     print(a['imgs'].shape)
     print(a['label'].shape)
-    print(a['img_metas'].data)
+    print(a['img_metas'].data[0][0]['filename'])
 
     if args.tensorrt:
         outputs = inference_tensorrt(args.checkpoint, distributed, data_loader,
