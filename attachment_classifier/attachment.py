395c3dcdecb1260f14542314672a377cbae98969
Fazry <lhuqita.fazry@gmail.com>
2022-12-14 20:53:59 +0700

    add more text

--- attachment_classifier/attachment.py
+++ attachment_classifier/attachment.py
@@ -116,8 +116,8 @@ class AttachmentClassifier(pl.LightningModule):
         return self.shared_step(batch, 'predict')
 
     def configure_optimizers(self):
-        optimizer = torch.optim.SGD(self.parameters(), lr=0.001, momentum=0.9, weight_decay=0.0005)
+        optimizer = torch.optim.Adam(self.parameters(), lr=0.001, momentum=0.9, weight_decay=0.0005)
         #optimizer = torch.optim.AdamW(self.parameters())
-        lr_scheduler = torch.optim.lr_scheduler.StepLR(optimizer, step_size=30, verbose=True)
+        lr_scheduler = torch.optim.lr_scheduler.StepLR(optimizer, step_size=30, verbose=True, gamma=0.1)
 
         return [optimizer], [lr_scheduler]
