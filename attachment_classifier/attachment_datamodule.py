b48606662deb626f2235e3dd0a0efcf43cc09f38
Fazry <lhuqita.fazry@gmail.com>
2022-12-14 07:18:25 +0700

    add new classifier

--- attachment_classifier/attachment_datamodule.py
+++ attachment_classifier/attachment_datamodule.py
@@ -9,12 +9,14 @@ from attachment_dataset import AttachmentDataset
 class AttachmentDataModule(pl.LightningDataModule):
     def __init__(self, data_dir: str = "data/attachments", 
             batch_size: int = 32, 
-            num_workers: int = 8):
+            num_workers: int = 8,
+            num_classes: int = 3):
         super().__init__()
 
         self.data_dir = data_dir
         self.batch_size = batch_size
         self.num_workers = num_workers
+        self.num_classes = num_classes
             
     def setup(self, stage = None):
         print(f'setup: {self.data_dir}')
@@ -23,27 +25,27 @@ class AttachmentDataModule(pl.LightningDataModule):
         if stage == "fit" or stage is None:
             self.train_set = AttachmentDataset(root=os.path.join(self.data_dir, 'train'), 
                 csv_file=os.path.join(self.data_dir, 'train.csv'), 
-                num_classes=8)
+                num_classes=self.num_classes)
             
             self.val_set   = AttachmentDataset(root=os.path.join(self.data_dir, 'test'), 
                 csv_file=os.path.join(self.data_dir, 'test.csv'), 
-                num_classes=8)
+                num_classes=self.num_classes)
 
         if stage == "validate" or stage is None:
             self.val_set   = AttachmentDataset(root=os.path.join(self.data_dir, 'test'), 
                 csv_file=os.path.join(self.data_dir, 'test.csv'), 
-                num_classes=8)
+                num_classes=self.num_classes)
 
         # Assign test dataset for use in dataloader(s)
         if stage == "test" or stage is None:
             self.val_set   = AttachmentDataset(root=os.path.join(self.data_dir, 'test'), 
                 csv_file=os.path.join(self.data_dir, 'test.csv'), 
-                num_classes=8)
+                num_classes=self.num_classes)
 
         if stage == "predict" or stage is None:
             self.val_set   = AttachmentDataset(root=os.path.join(self.data_dir, 'test'), 
                 csv_file=os.path.join(self.data_dir, 'test.csv'), 
-                num_classes=8)
+                num_classes=self.num_classes)
 
     def train_dataloader(self):
         return DataLoader(self.train_set, 
