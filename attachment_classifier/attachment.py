90dd2cda48bcbaad1de440e70099ec18015a9acd
Fazry <lhuqita.fazry@gmail.com>
2022-12-14 20:10:34 +0700

    add more text

--- attachment_classifier/attachment.py
+++ attachment_classifier/attachment.py
@@ -26,22 +26,22 @@ class AttachmentClassifier(pl.LightningModule):
         self.accuracy = MulticlassRecall(num_classes=num_classes)
         
         self.classifier = nn.Sequential(
-            nn.Dropout(p=0.2),
-            nn.BatchNorm1d(in_channels),
+            #nn.Dropout(p=0.2),
+            #nn.BatchNorm1d(in_channels),
             nn.Linear(in_features=in_channels, out_features=4*in_channels, bias=True),
             nn.ReLU(inplace=True),
 
-            nn.Dropout(p=0.2),
-            nn.BatchNorm1d(4*in_channels),
+            #nn.Dropout(p=0.2),
+            #nn.BatchNorm1d(4*in_channels),
             nn.Linear(in_features=4*in_channels, out_features=4*in_channels, bias=True),
             nn.ReLU(inplace=True),
 
-            nn.Dropout(p=0.2),
-            nn.BatchNorm1d(4*in_channels),
+            #nn.Dropout(p=0.2),
+            #nn.BatchNorm1d(4*in_channels),
             nn.Linear(in_features=4*in_channels, out_features=2*in_channels, bias=True),
             nn.ReLU(inplace=True),
 
-            nn.BatchNorm1d(2*in_channels),
+            #nn.BatchNorm1d(2*in_channels),
             nn.Linear(in_features=2*in_channels, out_features=num_classes)
         )
 
