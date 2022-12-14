e5de4ced961bc478fa9d048a7c8651bf7c8a602b
Fazry <lhuqita.fazry@gmail.com>
2022-12-14 20:47:19 +0700

    add more text

--- attachment_classifier/attachment.py
+++ attachment_classifier/attachment.py
@@ -118,6 +118,6 @@ class AttachmentClassifier(pl.LightningModule):
     def configure_optimizers(self):
         optimizer = torch.optim.Adam(self.parameters(), lr=1e-3, weight_decay=1e-4)
         #optimizer = torch.optim.AdamW(self.parameters())
-        lr_scheduler = torch.optim.lr_scheduler.StepLR(optimizer, step_size=30, gamma=0.1, verbose=True)
+        lr_scheduler = torch.optim.lr_scheduler.CosineAnnealingLR(optimizer)
 
         return [optimizer], [lr_scheduler]
