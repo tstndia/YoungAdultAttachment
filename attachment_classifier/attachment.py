d89b1a46a88fc4bbee5936acb58d790f4c25f038
Fazry <lhuqita.fazry@gmail.com>
2022-12-14 20:45:49 +0700

    add more text

--- attachment_classifier/attachment.py
+++ attachment_classifier/attachment.py
@@ -28,17 +28,17 @@ class AttachmentClassifier(pl.LightningModule):
         self.classifier = nn.Sequential(
             #nn.Dropout(p=0.2),
             #nn.BatchNorm1d(in_channels),
-            #nn.Linear(in_features=in_channels, out_features=4*in_channels, bias=True),
-            #nn.ReLU(inplace=True),
+            nn.Linear(in_features=in_channels, out_features=4*in_channels, bias=True),
+            nn.ReLU(inplace=True),
 
             #nn.Dropout(p=0.2),
             #nn.BatchNorm1d(4*in_channels),
-            #nn.Linear(in_features=4*in_channels, out_features=4*in_channels, bias=True),
-            #nn.ReLU(inplace=True),
+            nn.Linear(in_features=4*in_channels, out_features=4*in_channels, bias=True),
+            nn.ReLU(inplace=True),
 
             #nn.Dropout(p=0.2),
             #nn.BatchNorm1d(4*in_channels),
-            nn.Linear(in_features=in_channels, out_features=2*in_channels, bias=True),
+            nn.Linear(in_features=4*in_channels, out_features=2*in_channels, bias=True),
             nn.ReLU(inplace=True),
 
             #nn.BatchNorm1d(2*in_channels),
