13ee4639fcebe1f2307f59523f66a5bc147b677a
Fazry <lhuqita.fazry@gmail.com>
2022-12-15 16:32:12 +0700

    add new config

--- attachment_classifier/attachment.py
+++ attachment_classifier/attachment.py
@@ -29,22 +29,22 @@ class AttachmentClassifier(pl.LightningModule):
             nn.Dropout(p=0.2),
             nn.BatchNorm1d(in_channels),
             nn.Linear(in_features=in_channels, out_features=8*in_channels, bias=True),
-            nn.ReLU(inplace=True),
+            nn.LeakyReLU(inplace=True),
 
             nn.Dropout(p=0.2),
             nn.BatchNorm1d(8*in_channels),
             nn.Linear(in_features=8*in_channels, out_features=8*in_channels, bias=True),
-            nn.ReLU(inplace=True),
+            nn.LeakyReLU(inplace=True),
 
             nn.Dropout(p=0.2),
             nn.BatchNorm1d(8*in_channels),
             nn.Linear(in_features=8*in_channels, out_features=4*in_channels, bias=True),
-            nn.ReLU(inplace=True),
+            nn.LeakyReLU(inplace=True),
 
             nn.Dropout(p=0.2),
             nn.BatchNorm1d(4*in_channels),
             nn.Linear(in_features=4*in_channels, out_features=2*in_channels, bias=True),
-            nn.ReLU(inplace=True),
+            nn.LeakyReLU(inplace=True),
 
             nn.BatchNorm1d(2*in_channels),
             nn.Linear(in_features=2*in_channels, out_features=num_classes)
