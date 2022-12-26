4dbe8b85c8953dd41c4335abe7bb0d70d61c87c8
Fazry <lhuqita.fazry@gmail.com>
2022-12-27 05:48:26 +0700

    add more config

--- configs/recognition/attachment/exposure/exposure_tiny_cl128_fi2.py
+++ configs/recognition/attachment/exposure/exposure_tiny_cl128_fi2.py
@@ -1,5 +1,5 @@
 _base_ = [
-    './configs/_base_/models/swin/swin_tiny.py', './configs/_base_/default_runtime.py'
+    '../../../_base_/models/swin/swin_tiny.py', '../../../_base_/default_runtime.py'
 ]
 
 model=dict(
