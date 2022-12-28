7c5389af60a10400ab28fe6eda1961974f5670c3
tustynadia <tustynadia@gmail.com>
2022-12-28 20:32:40 +0700

    row 13

---
+++ configs/recognition/attachment/fusions/fusion_vs_cl1024_fi2_as_cl256_fi8.py
@@ -0,0 +1,14 @@
+exposure = dict(
+    config='configs/recognition/attachment/attachment_exposure_small_cl128_fi2.py',
+    checkpoint='work_dirs/rerun_attachment_exposure_small_cl128_fi2_b8_ep30/latest.pth'
+)
+
+video_response = dict(
+    config='configs/recognition/attachment/attachment_response_video_small_cl1024_fi2.py',
+    checkpoint='work_dirs/attachment_response_video_small_cl1024_fi2_b8_ep30/latest.pth'
+)
+
+audio_response = dict(
+    config='configs/recognition/attachment/attachment_response_audio_small_cl256_fi8.py',
+    checkpoint='work_dirs/attachment_response_audio_small_cl256_fi8_ep100/latest.pth'
+)
