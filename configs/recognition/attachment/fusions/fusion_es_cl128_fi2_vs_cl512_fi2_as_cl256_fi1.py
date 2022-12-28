8f6d9d5b00fe2e9648c32f3907a571a1d99b9687
tustynadia <tustynadia@gmail.com>
2022-12-28 14:51:48 +0700

    row10

---
+++ configs/recognition/attachment/fusions/fusion_es_cl128_fi2_vs_cl512_fi2_as_cl256_fi1.py
@@ -0,0 +1,14 @@
+exposure = dict(
+    config='configs/recognition/attachment/attachment_exposure_small_cl128_fi2.py',
+    checkpoint='work_dirs/rerun_attachment_exposure_small_cl128_fi2_b8_ep30/latest.pth'
+)
+
+video_response = dict(
+    config='configs/recognition/attachment/attachment_response_video_small_cl512_fi2.py',
+    checkpoint='work_dirs/attachment_response_small_cl512_fi2_ep30/latest.pth'
+)
+
+audio_response = dict(
+    config='configs/recognition/attachment/attachment_response_audio_small_cl256_fi1.py',
+    checkpoint='work_dirs/attachment_response_audio_small_cl256_fi1_ep100/latest.pth'
+)
