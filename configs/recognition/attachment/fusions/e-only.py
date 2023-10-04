6289ab67e9284e6897b6ecced7fe60d77227b3a6
tustynadia <tustynadia@gmail.com>
2023-10-04 21:26:50 +0700

    ablation exposure

---
+++ configs/recognition/attachment/fusions/e-only.py
@@ -0,0 +1,14 @@
+exposure = dict(
+    config='configs/recognition/attachment/exposure_base_cl512_fi2.py',
+    checkpoint='work_dirs/attachment_exposure_base_cl512_fi2_b8_ep30/latest.pth'
+)
+
+video_response = dict(
+    config='configs/recognition/attachment/attachment_response_video_small_cl128_fi4.py',
+    checkpoint='work_dirs/attachment_response_video_small_cl128_fi4_b8_ep30/latest.pth'
+)
+
+audio_response = dict(
+    config='configs/recognition/attachment/attachment_response_audio_small_cl256_fi4.py',
+    checkpoint='work_dirs/attachment_response_audio_small_cl256_fi4_ep100/latest.pth'
+)
